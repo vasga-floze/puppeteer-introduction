11f20b8c833d585b40f03740943ed0537dcea044
vasga-floze <gabriela.floze@gmail.com>
2024-05-13 21:31:09 -0600

    searching a selector and typing

--- __test__/navegador.test.js
+++ __test__/navegador.test.js
@@ -16,6 +16,16 @@ describe('Mi primer test en puppeteer', () => {
     });
     const page = await browser.newPage();
     await page.goto('https://www.instagram.com/');
+    await page.waitForSelector('#loginForm > div > div:nth-child(1) > div > label > input'); // Espera a que termine la navegación
+
+    const inputName = await page.waitForSelector('#loginForm > div > div:nth-child(1) > div > label > input'); // Ejemplo de selector
+    if (inputName) {
+      await inputName.type('vasga_floze'); // Solo escribe si el input existe
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+    } else {
+      console.error('El input para el nombre de usuario no se encontró');
+    }
+
     await browser.close();
   }, 60000); // Aumenta el tiempo de espera a 60 segundos
 });
